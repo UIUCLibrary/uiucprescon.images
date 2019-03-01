9960bb9ab29284ff88dbee38fcaffc6af0cff162
Borchers, Henry Samuel <hborcher@illinois.edu>
2019-03-01 16:25:24 -0600

    pep8

--- uiucprescon/imageprocess/__init__.py
+++ uiucprescon/imageprocess/__init__.py
@@ -1,1 +1,5 @@
 from .convert import convert_image
+
+__all__ = [
+    "convert_image"
+]
--- uiucprescon/imageprocess/convert.py
+++ uiucprescon/imageprocess/convert.py
@@ -1,13 +1,18 @@
+from typing import Dict, Union
 
-def convert_image(source, destination, type):
+output_types = {
+    "hathi", ""
+}
+
+
+def convert_image(source: str, output_file: str,
+                  output_type: Union[Dict[str, str]]) -> None:
     """Convert image from one file format into another
 
     Args:
-        source:
-        destination:
-        type:
-
-    Returns:
+        source: Source file to convert
+        output_file: File name to save new image
+        output_type: Desired output format
 
     """
     pass
