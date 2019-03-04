21bf19fc7a6cee4d7671fc3a67769e3b0c8183a7
Borchers, Henry Samuel <hborcher@illinois.edu>
2019-03-04 13:51:49 -0600

    corrected module name

--- tests/test_imageprocess.py
+++ tests/test_imageprocess.py
@@ -1,6 +1,6 @@
-from uiucprescon import imageprocess
+from uiucprescon import images
 
 
 def test_image_formats_loaded():
-    assert "Digital Library JPEG 2000" in imageprocess.image_formats
-    assert "HathiTrust JPEG 2000" in imageprocess.image_formats
+    assert "Digital Library JPEG 2000" in images.image_formats
+    assert "HathiTrust JPEG 2000" in images.image_formats
