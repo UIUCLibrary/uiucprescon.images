77d2590dfdf5ded4953792a9dbd0cffde1a59b6d
henry <henryborchers@yahoo.com>
2024-01-10 14:02:16 -0600

    removed pytest-runner from dev requirements

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
     packages=['uiucprescon.images'],
     namespace_packages=["uiucprescon"],
     install_requires=["pykdu_compress>=0.1.7b2"],
-    setup_requires=['pytest-runner'],
     test_suite="tests",
     tests_require=[
         'pytest',
