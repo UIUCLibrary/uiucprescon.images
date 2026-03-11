4e51ebf4863bf2961204a7e769c46b992ff9f475
Henry Borchers <henryborchers@yahoo.com>
2026-03-11 08:28:43 -0500

    ci: support testing of 3.14

--- vars/runJenkinsPipeline.groovy
+++ vars/runJenkinsPipeline.groovy
@@ -615,11 +615,11 @@ def call(){
                                 axes: [
                                     [
                                         name: 'PYTHON_VERSION',
-                                        values: ['3.10', '3.11', '3.12','3.13']
+                                        values: ['3.10', '3.11', '3.12', '3.13', '3.14']
                                     ],
                                     [
                                         name: 'OS',
-                                        values: ['linux','macos','windows']
+                                        values: ['linux', 'macos', 'windows']
                                     ],
                                     [
                                         name: 'ARCHITECTURE',
