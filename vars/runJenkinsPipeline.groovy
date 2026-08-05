4f8cd872dddd8ff17b1e1deb177091233e1ae1d3
hborcher <hborcher@illinois.edu>
2026-08-05 10:54:16 -0500

    ci: support a configuration file for Jenkins

--- vars/runJenkinsPipeline.groovy
+++ vars/runJenkinsPipeline.groovy
@@ -32,6 +32,14 @@ def get_sonarqube_unresolved_issues(report_task_file){
         return outstandingIssues
     }
 }
+@NonCPS
+def getExclusions(config){
+    (config['supporting']['exclusions'] ?: []).collect{ exclusion ->
+        return exclusion.collect{ component ->
+            return ["name": component['name'], "values": component['values']]
+        }
+    }
+}
 
 def installMSVCRuntime(cacheLocation){
     def cachedFile = "${cacheLocation}\\vc_redist.x64.exe".replaceAll(/\\\\+/, '\\\\')
@@ -58,7 +66,29 @@ def installMSVCRuntime(cacheLocation){
         powershell(label: 'Install VC Runtime', script: 'Start-Process -filepath "$Env:CACHED_FILE" -ArgumentList "/install", "/passive", "/norestart" -Passthru | Wait-Process;')
     }
 }
+
+def getConfig(){
+    def configData = [:]
+    node(){
+        checkout scm
+        def configID = 'uiucprescon_images_pipeline_config'
+        def defaultConfigFile = 'ci/jenkins/jenkins_config.json'
+        try{
+            configFileProvider([configFile(fileId: configID, variable: 'config_file')]) {
+                echo "Using configuration from: \"$configID\""
+                configData = readJSON( file: config_file)
+            }
+        } catch (e){
+            echo "Using default configuration in ${defaultConfigFile}. To override, create a new config file in Jenkins with id: \"${configID}\""
+            configData = readJSON( file: defaultConfigFile)
+        }
+    }
+    configData['supporting']['exclusions'] = getExclusions(configData)
+    return configData
+}
+
 def call(){
+    def config = getConfig()
     library(
         identifier: 'JenkinsPythonHelperLibrary@2024.12.0',
         retriever: modernSCM(
@@ -120,7 +150,7 @@ def call(){
                                     UV_TOOL_DIR='/tmp/uvtools'
                                     UV_PYTHON_CACHE_DIR='/tmp/uvpython'
                                     UV_CACHE_DIR='/tmp/uvcache'
-                                    UV_PYTHON = '3.12'
+                                    UV_PYTHON = "${config['default_python_version']}"
                                 }
                                 steps {
                                     catchError(buildResult: 'UNSTABLE', message: 'Building documentation produced an error or a warning', stageResult: 'UNSTABLE') {
@@ -176,7 +206,7 @@ def call(){
                                     UV_TOOL_DIR='/tmp/uvtools'
                                     UV_PYTHON_CACHE_DIR='/tmp/uvpython'
                                     UV_CACHE_DIR='/tmp/uvcache'
-                                    UV_PYTHON = '3.12'
+                                    UV_PYTHON = "${config['default_python_version']}"
                                 }
                                 stages{
                                     stage('Test') {
@@ -609,33 +639,22 @@ def call(){
                                 axes: [
                                     [
                                         name: 'PYTHON_VERSION',
-                                        values: ['3.10', '3.11', '3.12', '3.13', '3.14', '3.14t']
+                                        values: config['supporting']['pythonVersions']
                                     ],
                                     [
                                         name: 'OS',
-                                        values: ['linux', 'macos', 'windows']
+                                        values: config['supporting']['os']
                                     ],
                                     [
                                         name: 'ARCHITECTURE',
-                                        values: ['x86_64', 'arm64']
+                                        values: config['supporting']['architecture']
                                     ],
                                     [
                                         name: 'PACKAGE_TYPE',
                                         values: ['wheel', 'sdist'],
                                     ]
                                 ],
-                                excludes: [
-                                    [
-                                        [
-                                            name: 'OS',
-                                            values: 'windows'
-                                        ],
-                                        [
-                                            name: 'ARCHITECTURE',
-                                            values: 'arm64',
-                                        ]
-                                    ]
-                                ],
+                                excludes: config['supporting']['exclusions'],
                                 when: {entry -> "INCLUDE_${entry.OS}-${entry.ARCHITECTURE}".toUpperCase() && params["INCLUDE_${entry.OS}-${entry.ARCHITECTURE}".toUpperCase()]},
                                 stages: [
                                     { entry ->
